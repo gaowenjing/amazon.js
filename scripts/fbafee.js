8f0b6e3482d6196b357bff81a2de716b9ef7382f
james <james@dyip.cn>
2023-04-13 11:50:14 +0800

    new file:   scripts/fbafee.js 	new file:   userscripts/fbafee.user.js

---
+++ scripts/fbafee.js
@@ -0,0 +1,22 @@
+const sleep = async ms => new Promise(res => setTimeout(res, ms))
+
+const el = document.createElement('div')
+document.body.prepend(el)
+el.innerHTML = `
+<button id="aba_download">start download</button>
+<div id="msgboard"></div>
+`
+const getfbafee = () => {
+    const msgboard = document.querySelector('msgboard')
+    let msg =''
+    document.querySelector('kat-table').querySelectorAll('kat-table-row[data-cy]').forEach(
+        e => {
+            msg += e.innerText.match(/ASIN:\s(\w{10})/)[1] + ' ' + e.innerText.match(/\$\s*(\d+\.\d+)\sFBA Fee/)[1]
+        }
+    )
+    msgboard.innerHTML = msg
+}
+
+document.querySelector('#aba_download').onclick = () => {
+    getfbafee()
+}
---
+++ userscripts/fbafee.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         fba fee
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        *
+// @icon         https://www.google.com/s2/favicons?domain=dyip.cn
+// @updateURL    https://dyip.cn:8443/userscripts/template.user.js
+// @downloadURL    https://dyip.cn:8443/userscripts/template.user.js
+// @grant        none
+// ==/UserScript==
+
+
+const el = document.createElement('script')
+el.setAttribute('src', 'https://dyip.cn:8443/scripts/fbafee.js')
+
+document.body.append(el)
