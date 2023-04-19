513be7718131bca89512c80560670abf00b4db29
root <root@dyip.cn>
2023-04-19 18:12:04 +0800

    modified:   scripts/fbafee.js

--- scripts/fbafee.js
+++ scripts/fbafee.js
@@ -1,17 +1,32 @@
-const sleep = async ms => new Promise(res => setTimeout(res, ms))
+let el = document.createElement('div') 
 
-const el = document.createElement('div')
 document.body.prepend(el)
+el.style.position = 'fixed'
+el.style.bottom = '2em'
+el.style.right = '2em'
+el.style.zIndex = 100
 el.innerHTML = `
-<button id="aba_download">start download</button>
-<div id="msgboard"></div>
+<button id="aba_download">get fee</button>
+<textarea id="msgboard"></textarea>
+<div id="debugmsg"></div>
 `
-const getfbafee = () => {
+document.querySelector('#msgboard').ondblclick = () => {
+    let textarea = document.querySelector('#msgboard')
+    textarea.select()
+    document.execCommand('copy')
+}
+let  getfbafee = () => {
     const msgboard = document.querySelector('#msgboard')
+    const debugmsg = document.querySelector('#debugmsg')
     let msg =''
     document.querySelector('kat-table').querySelectorAll('kat-table-row[data-cy]').forEach(
         e => {
-            msg += e.innerText.match(/ASIN:\s(\w{10})/)[1] + ' ' + e.innerText.match(/\$\s*(\d+\.\d+)\sFBA Fee/)[1] +'<br>'
+            //console.log(e)
+try {
+            msg += e.innerText.match(/ASIN:\s(\w{10})/)[1] + ' ' + e.innerText.match(/\$\s*(\d+\.\d+)\s(FBA Fee|亚马逊物流费用)/)[1] +'\n'
+} catch (err) {
+    debugmsg.innerHTML += err
+}
         }
     )
     msgboard.innerHTML = msg
